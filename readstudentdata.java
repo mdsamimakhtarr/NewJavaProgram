5c849153c509b288ed57332c91bfc44a5a3f8a16
md samim akhtar <mdsamimakhtar0000@gmail.com>
2025-06-11 21:01:21 +0530

    Add readstudentdata.java

---
+++ readstudentdata.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java .io.ObjectInputStream;
+import java.io.FileInputStream;
+
+public class readstudentdata {
+  public static void main(String[] args)  throws IOException, ClassNotFoundException {
+    FileInputStream fis =new FileInputStream("Srusti.txt");
+    ObjectInputStream ois = new ObjectInputStream(fis);
+
+    Srustistudentt s2= (Srustistudentt) ois.readObject();
+    System.out.println(s2.toString());
+    System.out.println("Data Read Successfully");
+
+  }
+  
+}
