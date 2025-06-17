c44d8cc75f053117a916ba7bf4023ec3f2b2bbb5
md samim akhtar <mdsamimakhtar0000@gmail.com>
2025-06-17 09:34:07 +0530

    Method Level Syncronization

---
+++ methodLevelsynchronization.java
@@ -0,0 +1,38 @@
+class Table{
+    public  synchronized void  printTable(int n){
+        for(int i=1;i<=10;i++){
+            System.out.println(n*i);
+        }
+    }
+}
+class thread3 extends Thread{
+    Table t;
+    thread3(Table t){
+        this.t=t;
+    }
+    public void run(){
+        t.printTable(5);
+    }
+}
+class thread4 extends Thread{
+    Table t;
+    thread4(Table t){
+
+        this.t=t;
+    }
+    public void run(){
+
+        t.printTable(6);
+    }
+}
+public class methodLevelsynchronization {
+    public static void main(String[] args) {
+        Table obj=new Table();
+        thread3 t3=new thread3(obj);
+        thread4 t4=new thread4(obj);
+        t3.start();
+        
+        t4.start();
+
+    }
+}
