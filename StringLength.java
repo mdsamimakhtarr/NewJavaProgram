33f23c89fc79e91f568d03b76e36186f64e6f359
md samim akhtar <mdsamimakhtar0000@gmail.com>
2025-06-22 20:11:40 +0530

    String lenth()

---
+++ StringLength.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class StringLength{
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter The String");
+    String name=sc.nextLine();
+    System.out.println(name.length());
+
+
+    // length method returns Integer Method And it returns the length of the String
+  }
+}
