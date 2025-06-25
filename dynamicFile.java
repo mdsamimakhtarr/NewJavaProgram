31adc332ac3e6de61ce1f19421107b9bd1fa00b7
md samim akhtar <mdsamimakhtar0000@gmail.com>
2025-06-25 21:14:43 +0530

    Dynamic FileHandling Employee

---
+++ dynamicFile.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.io.IOException;
+
+class Employee implements Serializable{
+  int id;
+  String Name;
+  String JobRole;
+
+  public void setStudent(int id,String Name,String JobRole){
+    this.id=id;
+    this.Name=Name;
+    this.JobRole=JobRole;
+  }
+  public void getDetails(){
+    System.out.println("Employee id is "+id);
+    System.out.println("Employee Name is "+Name);
+    System.out.println("Employee JobRole is "+JobRole);
+  }
+  public String toString(){
+    return "Employee  is :\n"+id+
+    " Employee Name :"+Name+
+    "Employee job role :"+JobRole;
+  }
+}
+public class dynamicFile {
+  public static void main(String[] args) throws IOException {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the Employee Number");
+    int id=sc.nextInt();
+    sc.nextLine();
+    System.out.println("Enter the Employee Name");
+    String name=sc.nextLine();
+    System.out.println("Enter the job Role");
+    String job=sc.nextLine();
+
+    Employee e1=new Employee();
+    e1.setStudent(id, name, job);
+    FileOutputStream fos=new FileOutputStream("Gulu.txt");
+    ObjectOutputStream oos=new ObjectOutputStream(fos);
+    oos.writeObject(e1);
+    oos.flush();
+    oos.close();
+    sc.close();
+    System.out.println("Data Submitted Successfully");
+
+  }
+}
