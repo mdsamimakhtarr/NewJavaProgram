1999ffb356f4dc46be048bb055000ba3bc34282e
md samim akhtar <mdsamimakhtar0000@gmail.com>
2025-06-28 11:56:49 +0530

    Multiplication Table

---
+++ multiplication.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class multiplication {
+ static void mul(int n){
+  for(int i=1;i<=10;i++){
+    System.out.printf("%d * %d = %d\n",n,i,n*i);
+  }
+ }
+
+
+
+
+
+  public static void main(String[] args) {
+    mul(8);
+  }
+}
